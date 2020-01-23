d4f5535ccad9c8dc27af8c3305bd8777fee2e63a
kotarogasu <kouta2607@gmail.com>
2020-01-24 02:10:37 +0900

    Update application.rb

--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,6 @@ require 'rails/all'
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.assets.initialize_on_precompile = false
 module PrimevideoSerch
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
@@ -16,5 +15,6 @@ module PrimevideoSerch
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
     config.i18n.default_locale = :ja
+    config.assets.initialize_on_precompile = false
   end
 end
